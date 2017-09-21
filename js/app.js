76ee66653724743421604725679596f69a454e91
Marlos Pomin <marlospomin@gmail.com>
2017-09-21 19:21:39 -0300

    Add scroll event

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
 (function () {
-  // Your code goes here
+  $(window).scroll(function () {
+    var distanceY = window.pageYOffset || $(document).scrollTop(),
+      shrinkOn = 100;
+
+    if (distanceY > shrinkOn) {
+      $('.navbar').addClass('shrink');
+    } else {
+      $('.navbar').removeClass('shrink');
+    }
+  });
+
 })();
